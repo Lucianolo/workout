3b82e40567053b5127cfc20bd8f1f05276a9a76e
Luciano Ursache <luciano.ursache@gmail.com>
2017-02-22 15:40:13 +0000

    Add user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,13 @@ class User < ApplicationRecord
          
   has_many :exercises
   
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
   def full_name
     [first_name, last_name].join(" ")
     
   end
+  
+  
 end
