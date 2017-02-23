84216d47f76d7aaa2019025eceb7475035901072
Luciano Ursache <luciano.ursache@gmail.com>
2017-02-23 11:40:48 +0000

    Setup action_cable for real time chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,13 +5,17 @@ class MessagesController < ApplicationController
     @message = current_user.messages.build(message_params)
     @message.room = current_room
     
-    @message.save
-    redirect_to user_exercises_path(current_user)
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to user_exercises_path(current_user) }
+        format.js { ActionCable.server.broadcast "messages_room_#{current_room.id}", render(partial: 'shared/message', object: @message) }
+      end
+    end
   end
   
   private
   
-    def message_params
-      params.require(:message).permit(:body)
-    end
+  def message_params
+    params.require(:message).permit(:body)
+  end
 end
